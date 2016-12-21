91e4c4c37f057095c827ab6a354fb8451c09ace8
lc1024 <b02611002@ntu.edu.tw>
2016-12-22 01:44:10 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+<div id="app">
+    <h1>{{ msg }}</h1>
+
+    <hr>
+    
+    <p>// Played with Vuejs...</p>
+    <test></test>
+    
+    <hr>
+    
+    <p>// Played with discord widgt...</p>
+    <iframe src="https://discordapp.com/widget?id=253926765842399232&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+    
+    <hr>
+    
+    <p>// Played with Webhooks...</p>
+    
+    </div>
+</template>
+
+<script>
+    import Test from './components/Test.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Test
+        },
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+    
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">
+        <h1>Hello, World!!!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test'
+    }
+</script>
+
+<style scoped="true">
+    h1 {
+        border: 10px solid;
+    }
+</style>
